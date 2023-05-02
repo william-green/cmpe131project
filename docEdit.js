14c47b7460273592a058526718169177b2583ac4
haomiao wang <haomiao.wang@sjsu.edu>
2023-05-02 02:21:44 -0700

    111

--- docEdit.js
+++ docEdit.js
@@ -1,6 +1,22 @@
+let myFont;
+let logo;
+
+
+
 function format(command, value) {
     document.execCommand(command, false, value);
 }
+
+
+function preload(){
+  myFont=loadFont('orange juice 2.0.ttf');
+  logo=loadImage('logo.png');
+
+function setup (){
+
+  image(logo, 375, 300);
+  }
+
 
 function changeFont() {
     const Font = document.getElementById('input-font').value;
